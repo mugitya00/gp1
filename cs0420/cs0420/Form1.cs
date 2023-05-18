0f443c04f15d18ab8fd9ce7ae26227f92dd2fd96
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-05-18 13:22:25 +0900

    Initial commit

---
+++ cs0420/cs0420/Form1.cs
@@ -0,0 +1,18 @@
+namespace cs0420
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //‚¨‚Í‚æ‚¤‚Æ•\Ž¦
+            MessageBox.Show("‚¨‚Í‚æ‚¤");
+            int a = 0;
+            int b = 01 / a;
+        }
+    }
+}
